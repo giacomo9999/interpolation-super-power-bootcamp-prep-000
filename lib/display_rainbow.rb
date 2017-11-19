83ef29095475a238c7d190dde52f30affece04a6
James Gary <bucket@earthlink.net>
2017-11-19 18:50:16 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,5 +1,4 @@
-def display_rainbow(color_arr)
-  str_out = ""
-  color_arr.each {|x| str_out += x[0].upcase + ": "+x+", "}
-  puts str_out.slice(0,str_out.length-2)
+def display_rainbow (col_arr)
+  str_out = "R: #{col_arr[0]}, O: #{col_arr[1]}, Y: #{col_arr[2]}, G: #{col_arr[3]}, B: #{col_arr[4]}, I: #{col_arr[5]}, V: #{col_arr[6]}"
+  puts str_out
 end
